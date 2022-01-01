e791d27a4b1b3cef067b1a3ad9bea1ed159f0b50
David Mayes <davidmayes@Davids-MacBook-Pro.local>
2021-12-31 17:17:01 -0700

    Adding descriptions

--- src/components/game-details.js
+++ src/components/game-details.js
@@ -1,22 +1,43 @@
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
+import { makeStyles } from '@mui/styles';
 
 export default function GameDetails() {
-    const { gameId } = useParams();
-    const [game, setGame] = useState();
-    console.log('gameId', gameId);
+  const classes = useStyles();
+  const { gameId } = useParams();
+  const [game, setGame] = useState();
+  console.log('gameId', gameId);
 
-    useEffect(() => {
-      axios.get(`https://api.boardgameatlas.com/api/search?ids=${gameId}&client_id=${process.env.REACT_APP_BOARD_GAME_VAL}`).then((response) => {
-        console.log(response.data.games[0]);
-        setGame(response.data.games[0]);
-      });
-    }, [gameId])
+  useEffect(() => {
+    axios.get(`https://api.boardgameatlas.com/api/search?ids=${gameId}&client_id=${process.env.REACT_APP_BOARD_GAME_VAL}`).then((response) => {
+      console.log(response.data.games[0]);
+      setGame(response.data.games[0]);
+    });
+  }, [gameId])
 
-    return (
-        <div>
-          <img src={game?.images.medium}/>
+  return (
+      <div className={classes.mainDetails}>
+        <img src={game?.images.medium}/>
+
+        <div className={classes.sideInfo}>
+          <div className={classes.title}>{game?.name}</div>
         </div>
-    )
+      </div>
+  )
 }
+
+const useStyles = makeStyles(theme => ({
+  mainDetails: {
+    display: 'flex',
+  },
+  sideInfo: {
+    display: 'flex',
+    flexDirection: 'column',
+    padding: theme.spacing(2)
+  },
+  title: {
+    fontSize: '1.2em',
+    fontWeight: 'bold'
+  }
+}));
