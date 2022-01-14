010efdc048c3790a1ff5499ac3cc2f3c95b7c052
David Mayes <davidrmayes@gmail.com>
2022-01-13 21:22:50 -0700

    Created gamesByCatMap

--- src/components/games-by-cat-auto.js
+++ src/components/games-by-cat-auto.js
@@ -2,7 +2,8 @@ import { useEffect, useState } from 'react'
 import axios from "axios";
 
 export default function GamesByCatAuto() {
-  const defaultCats = ['Adventure', 'Family Game', 'Finance', 'Party Game', 'Card Game', 'Racing']
+  const defaultCatNames = ['Adventure', 'Family Game', 'Finance', 'Party Game', 'Card Game', 'Racing'];
+  const [gamesByCategory, setGamesByCategory] = useState({});
   
   const generateCatUrl = id => {
     return `https://api.boardgameatlas.com/api/search?categories=${id}&client_id=${process.env.REACT_APP_BOARD_GAME_VAL}`
@@ -10,16 +11,41 @@ export default function GamesByCatAuto() {
 
   useEffect(() => {
     axios.get(`https://api.boardgameatlas.com/api/game/categories?client_id=${process.env.REACT_APP_BOARD_GAME_VAL}`).then((response) => {
-      console.log(response.data);
-      const catRequests = response.data.categories
-        .filter(cat => defaultCats.includes(cat.name))
+      const defaultCats = response.data.categories.filter(cat => defaultCatNames.includes(cat.name));
+      console.log('defaultCats', defaultCats);
+      const gamesByCatMap = {};
+
+      defaultCats.forEach(cat => {
+        gamesByCatMap[cat.id] = {
+          id: cat.id,
+          name: cat.name,
+          games: []
+        }
+      })
+
+      setGamesByCategory(gamesByCatMap);
+
+      console.log('gamesByCategory1', gamesByCategory);
+
+      const catRequests = defaultCats
         .map(cat => cat.id)
         .map(id => axios.get(generateCatUrl(id)))
 
 
       axios.all(catRequests).then(axios.spread((...responses) => {
         console.log('responses', responses)
-      }))
+        const tempGamesByCategory = gamesByCategory;
+
+        for(const key in gamesByCategory) {
+          responses.forEach(response => {
+            if (response.config.url.includes(key)) {
+              tempGamesByCategory[key].games = response.data.games;
+            }
+          });
+        }
+
+        console.log('gamesByCategory2', gamesByCategory);
+      }));
     });
   }, []);
 
