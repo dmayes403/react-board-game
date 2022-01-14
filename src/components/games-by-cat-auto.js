09cf687acd8aa722c3854e9fd9503f69f4e9c09e
David Mayes <davidrmayes@gmail.com>
2022-01-13 21:28:06 -0700

    Refactored gamesByCatMap

--- src/components/games-by-cat-auto.js
+++ src/components/games-by-cat-auto.js
@@ -12,8 +12,7 @@ export default function GamesByCatAuto() {
   useEffect(() => {
     axios.get(`https://api.boardgameatlas.com/api/game/categories?client_id=${process.env.REACT_APP_BOARD_GAME_VAL}`).then((response) => {
       const defaultCats = response.data.categories.filter(cat => defaultCatNames.includes(cat.name));
-      console.log('defaultCats', defaultCats);
-      const gamesByCatMap = {};
+      let gamesByCatMap = {};
 
       defaultCats.forEach(cat => {
         gamesByCatMap[cat.id] = {
@@ -23,28 +22,22 @@ export default function GamesByCatAuto() {
         }
       })
 
-      setGamesByCategory(gamesByCatMap);
-
-      console.log('gamesByCategory1', gamesByCategory);
-
       const catRequests = defaultCats
         .map(cat => cat.id)
         .map(id => axios.get(generateCatUrl(id)))
 
-
       axios.all(catRequests).then(axios.spread((...responses) => {
-        console.log('responses', responses)
-        const tempGamesByCategory = gamesByCategory;
+        gamesByCatMap = gamesByCategory;
 
         for(const key in gamesByCategory) {
           responses.forEach(response => {
             if (response.config.url.includes(key)) {
-              tempGamesByCategory[key].games = response.data.games;
+              gamesByCatMap[key].games = response.data.games;
             }
           });
         }
 
-        console.log('gamesByCategory2', gamesByCategory);
+        setGamesByCategory(gamesByCatMap);
       }));
     });
   }, []);
